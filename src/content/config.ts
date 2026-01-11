7026b23a0b360dda8e99f69814a96187e98d062c
Quang Thang <thang@thangqt.com>
2026-01-12 03:57:06 +0700

    Rename blog to writing and add hometown post

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,12 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const writing = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
     date: z.date(),
     description: z.string().optional(),
+    tags: z.array(z.string()).optional(),
     cover: z.string().optional(),
     coverMode: z.enum(['inline', 'title-background']).optional(),
     contentBackground: z.string().optional(),
@@ -23,4 +24,4 @@ const projects = defineCollection({
   }),
 });
 
-export const collections = { blog, projects };
+export const collections = { writing, projects };
